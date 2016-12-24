e97ebe16702478bc7c0b0ab80ab5229eb65955e5
Benson Guo <benson1guo@gmail.com>
2016-12-23 21:55:23 -0500

    working on adding css

--- server.js
+++ server.js
@@ -49,6 +49,8 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.use('/cssFiles', express.static(__dirname+'/assets'));
+
 app.get('/', function(req,res){
 	res.sendFile('index.html',{root: path.join(__dirname, './html')});
 });
