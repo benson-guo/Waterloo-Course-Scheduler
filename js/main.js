03d4e592859d8d88a801e0d1ea62f943f9c6657a
Benson Guo <benson1guo@gmail.com>
2016-12-29 18:11:35 -0500

    provides visual coloured representation of course offerings upon submission

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $('form').on('submit',function(e){
 	e.preventDefault();
-	console.log("submitted");
-
+	for (var x=0; x<4; x++){
+		document.getElementById("o"+(x+1)).style.visibility = "hidden";
+	}
     $.ajax({
         type     : "POST",
         cache    : false,
@@ -9,6 +10,19 @@ $('form').on('submit',function(e){
         data     : $(this).serialize(),
         success  : function(data) {
         	console.log(data);
+        	if (data.length==0){
+        		document.getElementById("o1").style.visibility = "visible";
+        		return;
+        	}
+        	if (data.indexOf('F')>-1)
+        		document.getElementById("o2").style.visibility = "visible";
+        	if (data.indexOf('W')>-1)
+        		document.getElementById("o3").style.visibility = "visible";
+        	if (data.indexOf('S')>-1)
+        		document.getElementById("o4").style.visibility = "visible";
+        },
+        error    : function () {
+        	document.getElementById("o1").style.visibility = "visible";
         }
     });
 });
