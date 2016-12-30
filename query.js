370976c75c536dd954d1515adceeeb0c3da8f579
Benson Guo <benson1guo@gmail.com>
2016-12-30 17:50:52 -0500

    Switched to building term offerings from data of the last 3 terms

--- query.js
+++ query.js
@@ -1,5 +1,15 @@
 var methods = {};
 
+methods.getCourseList = function(uwclient,callback) {
+	uwclient.get('/courses.{format}',
+	{
+		format : 'json'
+	},
+	function(err,res){
+		callback(res['data']);
+	});
+};
+
 methods.getOfferings = function(uwclient,id,callback) {
 	uwclient.get('/courses/{course_id}.{format}',
 	{
@@ -7,8 +17,41 @@ methods.getOfferings = function(uwclient,id,callback) {
 		format : 'json'
 	},
 	function(err,res){
+		// console.log(res['data']);
 		callback(res['data']['terms_offered']);
 	});
 };
 
-exports.data = methods
+methods.getTermCourses = function(uwclient,term,callback) {
+	uwclient.get('/terms/{term_id}/courses.{format}',
+	{
+		term_id : term,
+		format : 'json'
+	},
+	function(err,res){
+		callback(res['data']);
+	});
+};
+
+methods.updateTermsOfferred = function(data,subjects,t){
+	for (var i in data){
+		var nextSub=data[i]['subject'];
+		var nextCN=data[i]['catalog_number'];
+		try{
+			subjects[nextSub][nextCN]['terms_offered'].push(t);
+		} catch (err){
+			if (!(nextSub in subjects)){
+				subjects[nextSub]={};
+			}
+			if (!(nextCN in subjects[nextSub])){
+				subjects[nextSub][nextCN]={};
+			}
+			subjects[nextSub][nextCN]['title']=data[i]['title'];
+			subjects[nextSub][nextCN]['course_id']=data[i]['course_id'];
+			subjects[nextSub][nextCN]['terms_offered']=[t];
+		}
+	}
+}
+
+
+exports.data = methods;
--- server.js
+++ server.js
@@ -5,38 +5,42 @@ var config = require('./config.json');
 var uwclient = new uwaterlooApi({
 	API_KEY : config.api_key
 });
+var response = require('./query.js');
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var app = express();
+var bodyParser = require('body-parser');
 
 var portTest = 9000;
 var port = 8080;
 
 var subjects={}
 
-uwclient.get('/courses.{format}',
-{
-	format : 'json'
-},
-function(err,res){
-	for (var i in res['data']){
-		var nextSub=res['data'][i]['subject'];
-		var nextCN=res['data'][i]['catalog_number'];
+response.data.getCourseList(uwclient,function(data){
+	for (var i in data){
+		var nextSub=data[i]['subject'];
+		var nextCN=data[i]['catalog_number'];
 		if (!(nextSub in subjects)){
 			subjects[nextSub]={};
 		}
 		if (!(nextCN in subjects[nextSub])){
 			subjects[nextSub][nextCN]={};
 		}
-		subjects[nextSub][nextCN]['title']=res['data'][i]['title'];
-		subjects[nextSub][nextCN]['course_id']=res['data'][i]['course_id'];
+		subjects[nextSub][nextCN]['title']=data[i]['title'];
+		subjects[nextSub][nextCN]['course_id']=data[i]['course_id'];
+		subjects[nextSub][nextCN]['terms_offered']=[];
 	}
+	response.data.getTermCourses(uwclient,config.FALL,function(data){
+		response.data.updateTermsOfferred(data,subjects,'F');
+	});
+	response.data.getTermCourses(uwclient,config.WINTER,function(data){
+		response.data.updateTermsOfferred(data,subjects,'W');
+	});
+	response.data.getTermCourses(uwclient,config.SPRING,function(data){
+		response.data.updateTermsOfferred(data,subjects,'S');
+	});
 });
-
-var response = require('./query.js');
-
-var express = require('express');
-var path = require('path');
-var fs = require('fs');
-var app = express();
-var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -45,16 +49,14 @@ app.use('/jsFiles', express.static(__dirname+'/js'));
 
 app.get('/', function(req,res){
 	res.sendFile('index.html',{root: path.join(__dirname, './html')});
-	// console.log(subjects);
 });
 
 app.post('/reqoff', function(req,res){
 	res.setHeader('Content-Type', 'application/json');
 	try{
 		var requestedID = subjects[req.body.courseSubject.toUpperCase()][req.body.courseCode.toUpperCase()]['course_id'];
-		response.data.getOfferings(uwclient,requestedID,function(data){
-			res.end(JSON.stringify(data));
-		});
+		console.log(subjects[req.body.courseSubject.toUpperCase()][req.body.courseCode.toUpperCase()]['terms_offered']);
+		res.end(JSON.stringify(subjects[req.body.courseSubject.toUpperCase()][req.body.courseCode.toUpperCase()]['terms_offered']));
 	} catch (err) {
 		res.end('No information found on '+req.body.courseSubject.toUpperCase()+req.body.courseCode.toUpperCase()+'.');
 	}
@@ -72,4 +74,5 @@ var listen = function(){
 	console.log('Listening!');
 }
 
-app.listen(1337,listen);
+app.listen(9000,listen);
+
